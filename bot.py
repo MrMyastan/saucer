e4e68307fbd57b9c3f5b0dcb6d915b64ebc1d745
MrMyastan <53984236+MrMyastan@users.noreply.github.com>
2021-08-12 14:12:12 -0500

    fuckin changes ig idk

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import discord
 from discord.ext import commands
 
 from NHentai.nhentai_async import NHentaiAsync
+from NHentai.entities.doujin import Doujin
 
 client = commands.Bot(description="Fetch the names of various sauces!",
                       pm_help=False,
@@ -18,7 +19,7 @@ nhentai = NHentaiAsync()
 config = None
 
 
-async def start(_config):
+async def start(_config: dict) -> None:
     global config
 
     config = _config
@@ -42,7 +43,7 @@ async def on_ready() -> None:
     #        "%d&scope=bot&permissions=19456" % client.user.id))
     print("Logged in!")
     print("Invite:  https://discordapp.com/oauth2/authorize?client_id=%d&scope=bot&permissions=19456" % client.user.id)
-    activity = discord.Game(name="Slurping up some yummy sauce!")
+    activity = discord.Game(name="!help | Slurping up some yummy sauce!")
     return await client.change_presence(activity=activity)
 
 @client.command()
@@ -50,8 +51,13 @@ async def sauce(context: commands.Context, id: str) -> None:
 
     if len(id) != 6 or id.isdecimal() != True:
         await context.send("Invalid sauce code. A valid code is a 6 digit number")
+        return
 
-    doujin = await nhentai.get_doujin(id=id)
+    doujin: Doujin = await nhentai.get_doujin(id=id)
+
+    if doujin is None:
+        await context.send(f"There is no sauce {id}. Sorry :/")
+        return
     
     await context.send(f"Sauce {id} is {doujin.title}")
 
